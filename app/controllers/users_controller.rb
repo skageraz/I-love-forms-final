3681180cb5f9b92a5ce3ebdbc45956dd511d91cf
Titouax <titouan.dessus@gmail.com>
2018-07-31 19:07:10 +0200

    les form du sale

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def new
+
   end
 
   def create
@@ -16,7 +17,39 @@ class UsersController < ApplicationController
     else
       redirect_to "/error"
     end
+
   end
+
+  def create2
+    puts params
+    user = User.new
+    user.username = params["username"]
+    user.email = params["email"]
+    user.bio = params["bio"]
+    user.save
+    if user.username.include?(" ")
+      redirect_to "/error2"
+    elsif user.id != nil
+      redirect_to "/users/#{User.last.username}"
+    else
+      redirect_to "/error"
+    end
+  
+  end
+
+  def create3
+   @user = User.create(username: params[:user][:username], email: params[:user][:email], bio: params[:user][:bio])
+   redirect_to "/users/#{User.last.username}"
+ end
+
+ def test
+   if @user.new_record? == false
+     redirect_to :action => “show”, :controller => “home”, :username => @user.username
+   else
+     redirect_to ‘/error’
+   end
+ end
+
 
   def show
     @username = params[:username]
@@ -27,4 +60,13 @@ class UsersController < ApplicationController
 
   def error
   end
+
+  def form_for
+    @user = User.new
+  end
+
+  def form_tag
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+
   get '/', to: 'static_pages#home'
   get '/contact', to: 'static_pages#contact'
   get '/about', to: 'static_pages#about'
@@ -7,12 +7,13 @@ Rails.application.routes.draw do
   get 'about/ani', to: 'static_pages#ani'
   get 'about/titouan', to: 'static_pages#titouan'
   get 'about/JR', to: 'static_pages#JR'
+
   get '/new', to: 'users#new'
-  get '/form_tag', to: 'users#new'
-  post '/form_tag', to: 'users#create'
+  get '/form_tag', to: 'users#form_tag'
+  post '/form_tag', to: 'users#create2'
   post '/new', to: 'users#create'
-  get '/form_for', to: 'users#new'
-  post '/form_for', to: 'users#create'
+  get '/form_for', to: 'users#form_for'
+  post '/form_for', to: 'users#create3'
   get '/error', to: 'users#error', as: 'error'
   get '/error2', to: 'users#error2', as: 'error2'
   get '/users/:username', to: 'users#show', as: 'show'
