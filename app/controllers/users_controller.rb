a0984ecf6d995f14db7ff3067c52a02f7d13aad8
skageraz <39991372+skageraz@users.noreply.github.com>
2018-08-01 02:08:57 +0200

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,23 @@
 class UsersController < ApplicationController
-  #Allez à la page création
+  #Amène à la page création
   def new
 
   end
 
   #Méthode Post du formulaire HTML
-  def create
+  def create 
     puts params
     user = User.new
     user.username = params["username"]
     user.email = params["email"]
     user.bio = params["bio"]
     user.save
-    if user.username.include?(" ")
-      redirect_to "/error2"
+    if user.username.include?(" ") #si le nom de l'utilisateur contient un espace, il est redirigé vers la page erreur
+      redirect_to "/error2" #technique qui permet de générer une page dynamique à partir du nom de l'user, si l'user_id n'est pas nul
     elsif user.id != nil
       redirect_to "/users/#{User.last.username}"
     else
-      redirect_to "/error"
+      redirect_to "/error" #sinon, l'utilisateur est redirigé vers une page erreur
     end
   end
 
@@ -54,7 +54,7 @@ class UsersController < ApplicationController
    end
  end
 
- #Méthode affichage page profile nouvel utilisateur
+ #Méthode affichage page profil nouvel utilisateur
   def show
     @username = params[:username]
     @id = User.find_by(username: @username).id
